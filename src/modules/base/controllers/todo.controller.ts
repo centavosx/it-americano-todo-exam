d8aae2969194b703a2a0526cca0c294f0882083c
Vincent Lennuel Llanto <72075830+soulevil098123@users.noreply.github.com>
2023-03-22 00:45:14 +0800

    Created todo controller endpoints

--- src/modules/base/controllers/todo.controller.ts
+++ src/modules/base/controllers/todo.controller.ts
@@ -1,35 +1,35 @@
 import { Body, Controller, Get, Post, Delete } from '@nestjs/common';
 import { Parameter } from '../../../helpers';
 import { Param, Patch } from '@nestjs/common/decorators';
-import { UserService } from '../services';
-import { CreateUserDto } from '../dto';
+import { TodoService } from '../services';
+import { TodoDto } from '../dto';
 
 @Controller('todo')
 export class TodoController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly todoService: TodoService) {}
 
   @Get()
   public async getAllTodo() {
-    return await this.userService.getAllUser();
+    return await this.todoService.getAllTodos();
   }
 
   @Get(Parameter.id())
   public async getTodo(@Param('id') id: string) {
-    return await this.userService.getUser(id);
+    return await this.todoService.getTodo(id);
   }
 
   @Post()
-  public async createTodo(@Body() data: CreateUserDto) {
-    return await this.userService.createUser(data);
+  public async createTodo(@Body() data: TodoDto) {
+    return await this.todoService.createTodo(data);
   }
 
   @Patch(Parameter.id())
-  public async updateTodo(@Body() data: CreateUserDto) {
-    return await this.userService.createUser(data);
+  public async updateTodo(@Param('id') id, @Body() data: TodoDto) {
+    return await this.todoService.updateTodo(id, data);
   }
 
   @Delete(Parameter.id())
   public async deleteTodo(@Param('id') id: string) {
-    return await this.userService.deleteUser(id);
+    return await this.todoService.deleteTodo(id);
   }
 }
